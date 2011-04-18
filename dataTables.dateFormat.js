9af944cf19475319ea7a53e39e6e079a42ef006b
Hassaan Aamir <hassaan@hotmail.ca>
2011-04-18 14:11:31 -0700

    Added logic to handle the dd-mmm-yyyy date format.

--- dataTables.dateFormat.js
+++ dataTables.dateFormat.js
@@ -1,12 +1,19 @@
 function calculate_date(date) {
+
+	// Month abbreviations. Change this for local month names
+	var monthAbbreviations = new Array('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+
 	var date = date.replace(" ", "");
 	
 	if (date.indexOf('.') > 0) {
-		/*date a, format dd.mn.(yyyy) ; (year is optional)*/
+		/*date a, format dd.mm(.yyyy) ; (year is optional)*/
 		var eu_date = date.split('.');
-	} else {
-		/*date a, format dd/mn/(yyyy) ; (year is optional)*/
+	} else if(date.indexOf('/') > 0) {
+		/*date a, format dd/mm(/yyyy) ; (year is optional)*/
 		var eu_date = date.split('/');
+	} else {
+		/*date a, format dd-mmm(-yyyy) ; (year is optional)*/
+		var eu_date = date.split('-');
 	}
 	
 	/*year (optional)*/
@@ -18,10 +25,19 @@ function calculate_date(date) {
 	
 	/*month*/
 	var month = eu_date[1];
-	if (month.length == 1) {
-		month = 0+month;
+	if(!isNaN(month - 0)) {
+		if (month.length == 1) {
+			month = 0+month;
+		}
+	} else {	
+		for (var i = 0; i < monthAbbreviations.length; i++) {
+			var month_name = monthAbbreviations [i];
+			if (month.toLowerCase() == month_name.toLowerCase()) {
+				month = (i%12) + 1;
+			}
+		}
 	}
-	
+
 	/*day*/
 	var day = eu_date[0];
 	if (day.length == 1) {
